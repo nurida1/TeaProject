f459505f189eb51940de5c5218e38edde08998b0
nurida1 <17nurida@gmail.com>
2020-10-04 19:39:13 -0400

    commit 2

--- src/TeaClass.java
+++ src/TeaClass.java
@@ -1,7 +1,8 @@
 public class TeaClass {
     public static void main(String[] args) {
 
-        System.out.println("Tea with sugar");
+        System.out.println("Tea without sugar");
+
 
     }
 }
